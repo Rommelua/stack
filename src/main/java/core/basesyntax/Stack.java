7d3931c2b09b8a5b320c40d35bbdbd04bdf9948f
Leonid <rommel@ukr.net>
2020-08-18 09:32:29 +0300

    stack done

--- src/main/java/core/basesyntax/Stack.java
+++ src/main/java/core/basesyntax/Stack.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Stack {
+import core.basesyntax.exception.EmptyStackException;
 
+public class Stack<E> {
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
+
+    public Stack() {
+        elementData = new Object[INITIAL_CAPACITY];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public E push(E item) {
+        elementData[size++] = item;
+        if (size == elementData.length) {
+            grow();
+        }
+        return item;
+    }
+
+    public E pop() {
+        E item;
+        try {
+            item = (E) elementData[--size];
+        } catch (Exception e) {
+            throw new EmptyStackException("Stack is empty");
+        }
+        elementData[size] = null;
+        return item;
+    }
+
+    private void grow() {
+        int newCapacity = elementData.length * 2;
+        Object[] newElementData = new Object[newCapacity];
+        System.arraycopy(elementData, 0, newElementData, 0, elementData.length);
+        elementData = newElementData;
+    }
 }
---
+++ src/main/java/core/basesyntax/exception/EmptyStackException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class EmptyStackException extends RuntimeException {
+    public EmptyStackException(String message) {
+        super(message);
+    }
+}
